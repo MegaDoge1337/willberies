a0b0e9f0d70101eb40349044bf47c79f32d757a1
MegaDoge1337 <56873848+MegaDoge1337@users.noreply.github.com>
2021-01-30 11:01:56 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
+
   direction: 'horizontal',
   loop: true,
 
-  // Navigation arrows
   navigation: {
     nextEl: '.slider-button-next',
     prevEl: '.slider-button-prev',
 
+  },
+
+  autoplay: {
+    delay: 5000,
   }
+
 });
